32e6a66951281f6afdf381755bb036d87ef4674d
Willie Jones <williejones432@yahoo.com>
2024-02-08 17:54:32 -0600

    created portfolio file and Projects data file. add placeholder images to display for styling

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import React from 'react';
 import { Header } from './components/Header';
-import { About } from './components/About';
+// import { About } from './components/About';
+import { Portfolio } from './components/Portfolio';
 
 function App() {
   
   return (
     <div className="App">
       <Header />
-      <About />
+      <Portfolio />
     </div>
   );
 }
--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -1,8 +1,24 @@
+import Projects from './Projects';
+
 export const Portfolio = () => {
+
+    const projects = Projects.map((project, index) => {
+        return (
+            <div key={index} className="project">
+                <div className="project-image">
+                    <img src={project.image} alt={project.name} />
+                </div>
+                <div className="project-info">
+                    <p>{project.name}</p>
+                    <p>{project.description}</p>
+                </div>
+            </div>
+        )
+    })
+
     return (
-        <div id="Portfolio">
-            <h1>Portfolio</h1>
-            <p>Projects will be added here</p>
+        <div id="Portfolio" className="layout">
+            {projects}
         </div>
     )
 }
