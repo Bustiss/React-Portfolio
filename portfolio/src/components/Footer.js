3870865741ee27e95169de716d59ce3f0a591c69
Willie Jones <williejones432@yahoo.com>
2024-02-13 00:10:32 -0600

    updated job container for Resume file

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,5 +1,6 @@
 import { FaGithub } from 'react-icons/fa';
 import { FaLinkedin } from 'react-icons/fa';
+import { FaYoutube } from 'react-icons/fa';
 import '../App.css'; // Imports App.css for styling
 
 export const Footer = () => {
@@ -12,8 +13,9 @@ export const Footer = () => {
 
     return (
         <footer className='footer'>
-                <a style={iconStyle} href="https://github.com/Liiv-Dev"><FaGithub /></a>
-                <a style={iconStyle} href="www.linkedin.com/in/williejones32"><FaLinkedin /></a>
+                <a style={iconStyle} href="https://github.com/Liiv-Dev" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+                <a style={iconStyle} href="www.linkedin.com/in/williejones32" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+                <a style={iconStyle} href="https://www.youtube.com/@Liiv_Dev-nw9ir" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
         </footer>
     )
 };
--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -1,17 +1,28 @@
 export const Resume = () => {
-    
-    const backGround = {
-        backgroundColor: 'lightgrey',
-        padding: '20px',
-        margin: '20px',
-        borderRadius: '10px',
-        boxShadow: '5px 5px 5px black'
-    }
 
     return (
-        <div style={backGround}>
-            <h1>Resume</h1>
-            <p>My resume will go here</p>
+        <div className="resume-page">
+            <div className="resume-container">
+                <div className="job-container">
+                    <h2>Job Experience</h2>
+
+                    <h3>RenewAire LLC - Waunakee, WI</h3>
+                    <p>Job Title: Commercial Tech</p>
+                    <p>Dates: Dec 2022 - Current
+                    <p>Job Description: Build ventilation systems, Wire electrical components (unit & controllers) according to schematic layouts, Run  HiPot  tests  and  perform  live  electrical  tests  to  ensure  units are running properly</p>
+                    </p>
+
+                    <h3>ThermaStor - Madison, WI</h3>
+                    <p>Job Title: Refrigeration Tech</p>
+                    <p>Dates: July 2020 - Apr 2022
+                    <p>Job Description: Charge units with refrigerant, Braze & Assemble dehumidifiers, build electrical boxes, run hi-pot current test, final quality checks on units, box unites for shipping</p>
+                    </p>
+                </div>
+
+                <div className="education-container">
+                    <h2> Education </h2>
+                </div>
+            </div>
         </div>
     )
 }
