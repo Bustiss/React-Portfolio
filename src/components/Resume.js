b99b70bc063b1e1e3d43795d61a1214384a89d9a
Willie Jones <williejones432@yahoo.com>
2024-03-29 20:11:18 -0500

    Course Update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -100,7 +100,7 @@ export const Resume = () => {
                     </div>
 
                     <h3>Codecademy - Online</h3>
-                    <p><h4>Course</h4>~ Computer Science </p>
+                    <p><h4>Course</h4>~ IOS Development </p>
                     <div className="skill-wrapper">
                         <Skill className="Ed-Graph" Icon={IoIosSchool} skillName="CSS3" progress={40} color="white" />
                     </div>
