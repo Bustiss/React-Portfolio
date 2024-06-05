8c4a938d9918ef0cf4c509cd5b981f53e2b9b5d9
Willie Jones <williejones432@yahoo.com>
2024-06-05 09:55:04 -0500

    disabled email inputs

--- src/components/EmailConfig.jsx
+++ src/components/EmailConfig.jsx
@@ -29,12 +29,12 @@ export const EmailConfig = () => {
         <div className="form-container">
             <form className="contact-form" ref={form} onSubmit={sendEmail}>
                 <label for="name">Name:</label>
-                <input type="text" id="name" name="name" required></input>
+                <input type="text" id="name" name="name" required disabled></input>
                 <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required></input>
+                <input type="email" id="email" name="email" required disabled></input>
                 <label for="message">Message:</label>
-                <textarea id="message" name="message" required></textarea>
-                {emailSent ? <p style={{ color: 'cyan', fontSize: '20px' }}>Email sent!</p> : <input type="submit" value="Send"></input>} {/* Display submit button or "Email sent" message */}
+                <textarea id="message" name="message" required disabled></textarea>
+                {emailSent ? <p style={{ color: 'cyan', fontSize: '20px' }}>Email sent!</p> : <input type="submit" value="Send" required disabled></input>} {/* Display submit button or "Email sent" message */}
             </form>
         </div>    
     )
